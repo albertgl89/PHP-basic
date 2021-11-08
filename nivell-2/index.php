883d550a4fb11815ff0ba51898bf8518e62f5371
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-08 14:43:51 +0000

    Nivell 2 / Exercici 2 finalitzat

--- nivell-2/index.php
+++ nivell-2/index.php
@@ -16,3 +16,27 @@ echo suma(2,2)."<br>"; //Esperat: 8
 
 echo "<br>";
 
+echo "<h1>Exercici 2</h1>";
+
+function inverteix($text){
+    if (strlen($text) <= 1){
+        return $text;
+    } else if (strlen($text) == 2){
+        $array = str_split($text);
+        return $array[1].$array[0]; 
+    } else {
+        $array = str_split($text);
+        $primerCaracter = $array[0];
+        $ultimCaracter = $array[count($array)-1];
+        $cosText = [];
+        for ($i = 1; $i < count($array)-1; $i++){
+            array_push($cosText, $array[$i]);
+        }
+        $cosText = implode("",$cosText);
+        return $ultimCaracter.$cosText.$primerCaracter;
+    }
+}
+
+echo inverteix('wxyz')."<br>";//Esperat: zxyw
+echo inverteix('a')."<br>"; //Esperat: a
+echo inverteix('ab')."<br>"; //Esperat ba
