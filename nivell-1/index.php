fb1a26ca6221da0b6534f2ab8c902a7e3ce72d54
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-08 11:55:09 +0000

    Nivell 1 / Exercici 1 finalitzat

---
+++ nivell-1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1>Exercici 1</h1>";
+
+$variableInteger = 4;//Integer
+$variableDouble = 5.6; //Double
+$variableString = "Hola :-D"; //String
+$variableBoolean = false; //Boolean
+
+echo $variableInteger."<br>";
+echo $variableDouble ."<br>";
+echo $variableString ."<br>";
+echo $variableBoolean ? "true" : "false";
