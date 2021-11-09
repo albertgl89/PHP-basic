38eeca26485b51e989f84b9dc41727d2d3766029
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-09 09:35:33 +0000

    Nivell 3 / Exercici 2 finalitzat

--- nivell-3/index.php
+++ nivell-3/index.php
@@ -22,3 +22,36 @@ function converteixStrEnArray($text){
 }
 
 print_r(converteixStrEnArray("  ABCDE TRE \n TROWP  "));//Test. Esperat: Array ( [0] => A [1] => B [2] => C [3] => D [4] => E [5] => T [6] => R [7] => E [8] => T [9] => R [10] => O [11] => W [12] => P )
+
+echo "<br>";
+
+echo "<h1>Exercici 2</h1>";
+
+/**
+ * Compta el nombre de vegades que un valor apareix en un array.
+ * @param array Array a on buscar el valor
+ * @param any Valor a buscar
+ * @return int Nombre de coincidències trobades
+ */
+function cercaCoincidencies($arr, $valor){
+    $comptador = 0;
+
+    foreach ($arr as $element){
+        if (strtolower($element) == strtolower($valor)){
+            $comptador++;
+        }
+    }
+
+    return $comptador;
+}
+
+//Tests
+$testArray = array("Hola", "Adeu", "Hola", "Hola", "Adeu", "Pere", "Casa", "Hola");
+$valor = "Hola";
+
+echo cercaCoincidencies($testArray, $valor)."<br>";//Esperat: 4
+
+$testArray = array(4, "Adeu", "Hola", "Hola", 4, "Pere", "Casa", 5);
+$valor = 4;
+
+echo cercaCoincidencies($testArray, $valor)."<br>";//Esperat: 2
